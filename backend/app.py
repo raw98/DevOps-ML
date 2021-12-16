5a1764347d18d62faf8812c98f479665a67efddb
raw98 <rawandbn98@gmail.com>
2021-12-16 03:26:37 +0100

    rm

--- backend/app.py
+++ backend/app.py
@@ -52,29 +52,6 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
            
 
-	
-@app.route('/uploader', methods = ['GET','POST'])
-def upload_file():
-   if request.method == 'GET':
-       return "hello"
-   if request.method == 'POST':
-         file = request.files['file']
-         if file.filename == '':
-              return 'No file selected'
-         if file and allowed_file(file.filename):
-          filename = secure_filename(file.filename)
-          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                   
-          signal, rate = librosa.load(UPLOAD_FOLDER+'/'+filename)  
-           #The Mel Spectrogram
-          S = librosa.feature.melspectrogram(signal, sr=rate, n_fft=2048,    hop_length=512, n_mels=128)
-          S_DB = librosa.power_to_db(S, ref=np.max)
-          S_DB = S_DB.flatten()[:1200]
-          clf = pickle.load(open('SVM.pkl' , 'rb'))
-          ans = clf.predict([S_DB])[0]
-          music_class = str(ans)
-          print(music_class)
-          return music_class
 
 @app.route('/uploadervgg', methods = ['GET','POST'])
 def classify_vgg():
--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
-app=Flask(__name__, template_folder='main.html')
+app=Flask(__name__, template_folder='interface')
 
 
-@app.route('/upload')
+@app.route('/service')
 def upload_files():
-   return render_template("form.html")
+   return render_template("main.html")
 
 		
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080) 
+    app.run(host="0.0.0.0", port=8081) 
